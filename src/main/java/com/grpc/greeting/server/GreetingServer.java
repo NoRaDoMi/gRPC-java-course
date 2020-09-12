6c4b0205bc0f9e49ec710fff302f10bb7dd9543e
Noradomi <fakemsi1999@gmail.com>
2020-09-12 16:33:35 +0700

    grpc ssl security: sefting signed certificate

--- src/main/java/com/grpc/greeting/server/GreetingServer.java
+++ src/main/java/com/grpc/greeting/server/GreetingServer.java
@@ -3,6 +3,7 @@ package com.grpc.greeting.server;
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
 
+import java.io.File;
 import java.io.IOException;
 
 public class GreetingServer {
@@ -10,7 +11,16 @@ public class GreetingServer {
   public static void main(String[] args) throws IOException, InterruptedException {
     System.out.println("Hello gRPC");
 
-    Server server = ServerBuilder.forPort(50051).addService(new GreetServiceImpl()).build();
+    //    plaintext server
+    //    Server server = ServerBuilder.forPort(50051).addService(new GreetServiceImpl()).build();
+
+    //    secure server
+    Server server =
+        ServerBuilder.forPort(50051)
+            .addService(new GreetServiceImpl())
+            .useTransportSecurity(new File("ssl/server.crt"), new File(("ssl/server.pem")))
+            .build();
+
     server.start();
 
     Runtime.getRuntime()
