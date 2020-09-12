b584b58a38a3638b42a526546dfa3068cdd7131c
Noradomi <fakemsi1999@gmail.com>
2020-09-12 17:18:07 +0700

    grpc reflection and evans cli

--- src/main/java/com/grpc/calculator/server/CalculatorServer.java
+++ src/main/java/com/grpc/calculator/server/CalculatorServer.java
@@ -2,13 +2,18 @@ package com.grpc.calculator.server;
 
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
+import io.grpc.protobuf.services.ProtoReflectionService;
 
 import java.io.IOException;
 
 public class CalculatorServer {
   public static void main(String[] args) throws IOException, InterruptedException {
     System.out.println("Hello gRPC");
-    Server server = ServerBuilder.forPort(5001).addService(new CalculatorServiceImpl()).build();
+    Server server =
+        ServerBuilder.forPort(5001)
+            .addService(new CalculatorServiceImpl())
+            .addService(ProtoReflectionService.newInstance())
+            .build();
     server.start();
 
     Runtime.getRuntime()
